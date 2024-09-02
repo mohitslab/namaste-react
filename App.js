40d9d797e251f3a9d8512ead0d0b65215931f9bb
Ranosys <mohitslab@gmail.com>
2024-09-02 07:16:51 +0530

    Episode 4
    - How to plan your React app by creating basic wireframe or mockup.
    - How distribute your app in various high-level components.
    - Identify the components required.
    - Use the **Component composition** to club multiple components together.
    - Add styling to the app.

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faShoppingCart, faSearch, faCircleArrowLeft, faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faShoppingCart, faSearch, faCircleArrowLeft, faCircleArrowRight, faStar } from "@fortawesome/free-solid-svg-icons";
 import logo from "./foodie.me-transparent-cropped.png"
 
-library.add(faUser, faShoppingCart, faCircleArrowLeft, faCircleArrowRight);
+library.add(faUser, faShoppingCart, faCircleArrowLeft, faCircleArrowRight, faStar);
 
 
 const Header = () => {
@@ -31,11 +31,33 @@ const Header = () => {
     )
 }
 
+const Dish = () => {
+    return (
+        <div className="dish">
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
+        </div>)
+}
+
+const Restaurant = () => {
+    return (
+        <div className="restaurant">
+            <div className="restaurant-image-container">
+                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/5/d11dfc30-3b77-4f98-bcb6-42e5032d7d59_528813.JPG" />
+            </div>
+            <div className="restaurant-info">
+                <p><strong>Bakingo</strong></p>
+                <span className="star-and-timing"><FontAwesomeIcon icon={faStar} className="star" /><strong>4.7 | 15-20 mins</strong></span>
+                <p>Bakery, Desserts, Beverages, Snacks</p>
+                <p>Mansarovar</p>
+            </div>
+        </div>)
+}
+
 const Body = () => {
     return <div className="body-container">
         <div className="dish-container-heading">
             <div className="dish-container-info">
-                <h3>Dear, What do you wanna try today?</h3>
+                <h2>Dear, What do you wanna try today?</h2>
             </div>
             <div className="dish-container-silder-arrow">
                 <FontAwesomeIcon icon={faCircleArrowLeft} size="2xl" />
@@ -43,53 +65,25 @@ const Body = () => {
             </div>
         </div>
         <div className="dish-container">
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
-            <div className="dish">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png" />
-            </div>
+            <Dish />
+            <Dish />
+            <Dish />
+            <Dish />
+            <Dish />
+            <Dish />
+            <Dish />
         </div>
         <div className="restaurant-container-info">
-            <h3>Top restaurants in the city</h3>
+            <h2>Top restaurants in the city</h2>
         </div>
         <div className="restaurant-container">
-            <div className="restaurant">
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/5/d11dfc30-3b77-4f98-bcb6-42e5032d7d59_528813.JPG"/>
-                <div>
-                    Bakingo
-                    4.7 | 15-20 mins
-                    Bakery, Desserts, Beverages. Snacks
-                    Mansarovar
-                </div>
-            </div>
+            <Restaurant />
+            <Restaurant />
+            <Restaurant />
+            <Restaurant />
+            <Restaurant />
+            <Restaurant />
+            <Restaurant />
         </div>
     </div>
 }
--- Episode-04-Talk-is-Cheap-Show-Me-the-Code/theory.md
+++ Episode-04-Talk-is-Cheap-Show-Me-the-Code/theory.md
@@ -1,9 +1,10 @@
 ## Steps to build a production ready app
 - First step to build any application should be **Planning**.
 - Planning is important and mandatory.
-- Step 1: Create rough mockups
+- Step 1: Create rough mockups or wireframe
     - Mockups are rough sketches which show a basic layout of your app.
-- Step 2: Identify components
+    - Purpose of wireframe is to know the exact placement of components before starting to code.
+- Step 2: Once basic wireframe is ready, try to identify components which should be created to build the proposed wireframe.
     - Generally there will be major 3 components viz. Header, Body and Footer.
     - Everything will be inside either of these three components.
 - Components:
@@ -26,6 +27,26 @@
         - Footer
             - MiniLogo
             - Copyright
-            - Company
+            - Address
             - ContactUs
             - ServiceLocations
+- Mockup
+![planning and basic wireframe](../basicWireframe.png)
+- We need to create the various components and use the Component composition to structure the app.
+- You can give style to components by targeting classes of the components or you can also use inline style tag.
+    - The inline style tag expects a javascript object.
+    - Provide inline css in an attribute named **style**.
+    - You need to give double-curly-brases.
+    - The outer curly brases indicates that a Javascript code is expected.
+    - Inner curly brases indicates the actual JSON.
+    - **Note:** Inline styles are not recommended.
+    - Example:
+```
+const RestaurantCard = () => {
+    return (
+        <div className="restaurant-card" style={ {
+            backgroundColor: "#ddd"
+        } }></div>
+    );
+}
+```
